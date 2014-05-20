1774aaf7c91453de0c0e6703a274fd0381002297
Juan Placencia <juan.placencia.512@gmail.com>
2014-05-19 21:00:52 -0500

    Add option to make observables hot

--- app/lib/store-from-observable.js
+++ app/lib/store-from-observable.js
@@ -1,14 +1,25 @@
 'use strict';
 
-module.exports = function(observable) {
-  return observable.asObservable().map(execute).map(wrap).concatAll();
+module.exports = function(observable, options) {
+  options = options || {};
+  observable = observable.asObservable().map(execute);
+  observable = options.forceHot ? observable.map(forceHot) : observable;
+  return observable.map(valueWrap).concatAll();
 };
 
 function execute(value) {
   return typeof value === 'function' ? Rx.Observable.defer(value) : value;
 }
 
-function wrap(value) {
+function forceHot(observable) {
+  if(observable instanceof Rx.Observable) {
+    observable = observable.publish();
+    observable.connect();
+  }
+  return observable;
+}
+
+function valueWrap(value) {
   return value instanceof Rx.Observable
     ? value
     : Rx.Observable.returnValue(value);
